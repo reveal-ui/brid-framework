b4fbb0221e26b0682812a1c90f574cffbd78fb22
Shawn Sandy <shawnsandy04@gmail.com>
2018-01-23 20:24:35 -0500

    Update helpers
    Add countUsers /  envSettings nethods

--- src/helpers.php
+++ src/helpers.php
@@ -1,20 +1,33 @@
 <?php
 
 use Rap2hpoutre\LaravelLogViewer\LaravelLogViewer;
+use Brotzka\DotenvEditor\DotenvEditor;
 
 
-if(!function_exists("countStories"))
-{
-
-	function countStories(){
-		return 10;
-	}
-
+if (!function_exists('countStories')) {
+    function countStories()
+    {
+        return 10;
+    }
 }
 
-if(!function_exists('errorLogs'))
-{
-	function errorLogs() {
-		return app(LaravelLogViewer::class);
+if(!function_exists('countUsers')){
+	function countUsers()
+	{
+		return App\User::count();
 	}
+}
+
+if (!function_exists('errorLogs')) {
+    function errorLogs()
+    {
+        return app(LaravelLogViewer::class);
+    }
+}
+
+if (!function_exists('envSettings')) {
+    function envSettings()
+    {
+        return app(DotenvEditor::class);
+    }
 }
