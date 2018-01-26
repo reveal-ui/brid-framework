c9b92e8674d4691d16e4a4f12be267ee98012d2f
Shawn Sandy <shawnsandy04@gmail.com>
2018-01-25 19:20:05 -0500

    Fix blade includes so they point to framework views

--- src/Classes/SystemLogs.php
+++ src/Classes/SystemLogs.php
@@ -8,15 +8,18 @@ class SystemLogs
 {
 
 	protected $logs;
+	protected $collection;
 
     public function __construct(LaravelLogViewer $logviewer){
 		$this->logs = $logviewer;
+		$this->collection = collect($this->logs->all());
 	}
 
 	public function getLogs()
 	{
-		return collect($this->logs->all())->take(30)->all();
+		$collection = $this->collection->take(30)
+		->all();
+		return $collection;
 	}
-
 
 }
